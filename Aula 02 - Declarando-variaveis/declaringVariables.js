99e0f0f625731394ce375dc97b02583bb31bb6c0
dev_lorrany <dev.lorranny@gmail.com>
2025-03-23 17:10:47 -0300

    Adicionando pasta da segunda aula do curso de frontend, na qual declaramos variaveis

---
+++ Aula 02 - Declarando-variaveis/declaringVariables.js
@@ -0,0 +1,39 @@
+// let const
+
+const primeiroValor = 10;
+const segundoValor = 20;
+console.log("primeiroValor + SegundoValor: ", primeiroValor+segundoValor);
+
+const valorTotal = primeiroValor + segundoValor;
+console.log("valorTotal: ", valorTotal);
+
+let terceiroValor = 50;
+console.log("terceiroValor: ", terceiroValor);
+
+terceiroValor = 100;
+console.log("terceiroValor final: ", terceiroValor);
+
+let comunidade = "Dev Completo";
+console.log('comunidade', comunidade);
+
+// const comunidade = 'Dev Completo';
+// const comunidade = `Dev Completo`; (acento ao contrário, consegue colocar o javascript dentro apartir do $)
+
+comunidade = "CDC";
+console.log('comunidade', comunidade);
+
+console.log(`Comunidade: ${comunidade}`);
+
+//padrão camelCase para declarar variaveis
+const autoraDoCurso = 'Beatriz Schwartz';
+//padrão snake_case
+const autora_do_curso = 'Beatriz Schwartz';
+
+//Unicos caracteres especiais que permite iniciar nomes de variaveis que são: $ e _, ex:
+const $palavra = '';
+const _palavra = '';
+
+let x = 10;
+x = "dez";
+
+
