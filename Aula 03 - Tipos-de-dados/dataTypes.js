6ea4b6ce838bd3fad8a9b62857a5369590238868
dev_lorrany <dev.lorranny@gmail.com>
2025-03-27 18:44:24 -0300

    Adicionando a pasta da Aula03 de tipos de dados

---
+++ Aula 03 - Tipos-de-dados/dataTypes.js
@@ -0,0 +1,17 @@
+
+const nome = "Lorrany"
+const idade = 23;
+let temCabeloCurto = false;
+console.log("Lorrany tem cabelo curto?", temCabeloCurto);
+
+console.log("5 meses depois........");
+
+
+temCabeloCurto = true;
+console.log("Lorrany tem cabelo curto?", temCabeloCurto);
+
+let profissao;
+console.log(profissao);
+
+profissao = "Programadora";
+console.log(profissao);
